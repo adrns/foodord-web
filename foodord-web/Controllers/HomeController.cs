54ef90faa330bd59a36fb231a76bcb1b83abf839
Sámson Adrián <samson.adrian.f@gmail.com>
2017-04-24 15:25:28 +0200

    added startup class and home controller

---
+++ foodord-web/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using foodord_web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace foodord_web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private FoodOrderingContext entities;
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
