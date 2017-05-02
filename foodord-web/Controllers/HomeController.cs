ab0eff712e5258a21097e8d674f9e3d7e5bd6edc
Sámson Adrián <samson.adrian.f@gmail.com>
2017-05-02 19:37:37 +0200

    created category action and links to it

--- foodord-web/Controllers/HomeController.cs
+++ foodord-web/Controllers/HomeController.cs
@@ -12,5 +12,17 @@ namespace foodord_web.Controllers
 
             return View("Index");
         }
+
+        // GET: Home/Category/?id
+        [HttpGet]
+        public ActionResult Category(int? id)
+        {
+            int categoryId = null == id ? 0 : (int)id;
+
+            ViewBag.Category = foodService.GetCategoryName(categoryId);
+            ViewBag.Foods = foodService.GetFoodsByCategory(categoryId);
+
+            return View("Category");
+        }
     }
 }
