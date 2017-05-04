947d5cb86b5a0ffb6ad91c5631bd04b356774786
Sámson Adrián <samson.adrian.f@gmail.com>
2017-05-04 17:51:59 +0200

    extract food id from button with jquery

---
+++ foodord-web/Content/scripts.js
@@ -0,0 +1,8 @@
+﻿"use strict";
+$(document).ready(function () {
+    console.log("ok, jquery works!");
+    $("button[data-basket='add']").click(function (event) {
+        let itemId = $(event.currentTarget).attr("data-food-id");
+        console.log(itemId);
+    });
+});
