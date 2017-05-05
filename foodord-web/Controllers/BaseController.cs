84a9f7965a777cf4caf0431a2e5e9a33961a6ff8
Sámson Adrián <samson.adrian.f@gmail.com>
2017-05-05 12:38:39 +0200

    share basket among BaseController subclasses

--- foodord-web/Controllers/BaseController.cs
+++ foodord-web/Controllers/BaseController.cs
@@ -6,10 +6,36 @@ namespace foodord_web.Controllers
     public class BaseController : Controller
     {
         protected FoodService foodService;
+        protected Basket basket;
 
         public BaseController()
         {
             foodService = new FoodService();
+        }
+
+        protected override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            base.OnActionExecuting(filterContext);
+
+            basket = CreateBasket();
+        }
+
+        protected override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            base.OnActionExecuted(filterContext);
+
+            Session["basket"] = basket;
+        }
+
+        private Basket CreateBasket()
+        {
+            object cached = Session["basket"];
+            if (cached != null && cached is Basket)
+            {
+                return (Basket)Session["basket"];
+            }
+
+            return new Basket(foodService);
         }
     }
 }
--- foodord-web/Controllers/BasketController.cs
+++ foodord-web/Controllers/BasketController.cs
@@ -5,33 +5,6 @@ namespace foodord_web.Controllers
 {
     public class BasketController : BaseController
     {
-        private Basket basket;
-
-        protected override void OnActionExecuting(ActionExecutingContext filterContext)
-        {
-            base.OnActionExecuting(filterContext);
-
-            basket = CreateBasket();
-        }
-
-        protected override void OnActionExecuted(ActionExecutedContext filterContext)
-        {
-            base.OnActionExecuted(filterContext);
-
-            Session["basket"] = basket;
-        }
-
-        private Basket CreateBasket()
-        {
-            object cached = Session["basket"];
-            if (cached != null && cached is Basket)
-            {
-                return (Basket)Session["basket"];
-            }
-
-            return new Basket(foodService);
-        }
-
         public ActionResult Add(int foodId)
         {
             switch (basket.Add(foodId))
