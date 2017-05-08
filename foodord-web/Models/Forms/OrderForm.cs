fc46a1f377bae0a4e668bc8ba88cbfbe99eb60ad
Sámson Adrián <samson.adrian.f@gmail.com>
2017-05-08 15:20:04 +0200

    added a model to the order form

---
+++ foodord-web/Models/Forms/OrderForm.cs
@@ -0,0 +1,11 @@
+﻿namespace foodord_web.Models.Forms
+{
+    public class OrderForm
+    {
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string ZipCode { get; set; }
+        public string County { get; set; }
+        public string Address { get; set; }
+    }
+}
